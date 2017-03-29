23fe2b9402a337c0517f39c1ff2919ae8fc9c220
Branimir Borisov <branimir@raketasoft.com>
2017-03-30 02:02:35 +0300

    refactor(server): fix tslint errors

--- demo/server.ts
+++ demo/server.ts
@@ -1,6 +1,6 @@
-import * as http from "http";
-import * as url from "url";
-import * as _ from "lodash";
+import * as http from 'http';
+import * as url from 'url';
+import * as _ from 'lodash';
 
 import DEMO_DATA from './data';
 
@@ -12,13 +12,13 @@ import DEMO_DATA from './data';
  * @author Branimir Borisov <branimir@raketasoft.com>
  * @since 1.0.0-beta.2
  */
-const HttpDispatcher = require("httpdispatcher");
+const HttpDispatcher = require('httpdispatcher');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 const dispatcher = new HttpDispatcher();
 
-dispatcher.onGet("/", function(request: any, result: any) {
+dispatcher.onGet('/', function(request: any, result: any) {
   const query: any = url.parse(request.url, true).query;
 
   const page: number = _.isUndefined(query.page) ? 1 : query.page;
@@ -37,9 +37,11 @@ dispatcher.onGet("/", function(request: any, result: any) {
 
   let filters: string[] = [];
   for (let filter in query) {
-    if (filter !== 'page' && filter !== 'pageSize' && filter !== 'orderBy'
-        && filter !== 'expand') {
-      filters[filter] = query[filter];
+    if (query.hasOwnProperty(filter)) {
+      if (filter !== 'page' && filter !== 'pageSize' && filter !== 'orderBy'
+          && filter !== 'expand') {
+        filters[filter] = query[filter];
+      }
     }
   }
 
@@ -49,10 +51,12 @@ dispatcher.onGet("/", function(request: any, result: any) {
   data = _.filter(data, function(item: any) {
     let match = true;
     for (let filter in filters) {
-      let value = _.get(item, filter).toString();
+      if (filters.hasOwnProperty(filter)) {
+        let value = _.get(item, filter).toString();
 
-      match = match &&
-        (value.match(new RegExp(filters[filter], 'i')) !== null);
+        match = match &&
+          (value.match(new RegExp(filters[filter], 'i')) !== null);
+      }
     }
 
     return match;
@@ -63,7 +67,7 @@ dispatcher.onGet("/", function(request: any, result: any) {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Expose-Headers': 'X-Pagination-Total-Count, X-Pagination-Page-Count, X-Pagination-Current-Page, X-Pagination-Per-Page',
     'X-Pagination-Total-Count': data.length,
-    'X-Pagination-Page-Count': Math.ceil(data.length/pageSize),
+    'X-Pagination-Page-Count': Math.ceil(data.length / pageSize),
     'X-Pagination-Current-Page': page,
     'X-Pagination-Per-Page': !_.isUndefined(pageSize) ? pageSize : ''
   });
