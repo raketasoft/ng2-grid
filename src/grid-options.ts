3ae8168845e163855503d81447039c783924b736
Branimir Borisov <b.borisov@devision.bg>
2017-08-29 10:02:33 +0300

    chore(tslint): fix lint rules

--- src/grid-options.ts
+++ src/grid-options.ts
@@ -12,25 +12,25 @@ import { StyleCallback } from './style-callback.interface';
  * @since 1.0.0-alpha.1
  */
 export class GridOptions extends Loadable {
-  static DEFAULT_FILTERING_VALUE: boolean = true;
-  static DEFAULT_HEADING_VALUE: boolean = true;
-  static DEFAULT_HEADING_FIXED_VALUE: boolean = false;
-  static DEFAULT_PAGE_BUTTON_COUNT_VALUE: number = 5;
-  static DEFAULT_PAGE_ELEMENT_POSITION_VALUE: string = 'left';
+  static DEFAULT_FILTERING_VALUE = true;
+  static DEFAULT_HEADING_VALUE = true;
+  static DEFAULT_HEADING_FIXED_VALUE = false;
+  static DEFAULT_PAGE_BUTTON_COUNT_VALUE = 5;
+  static DEFAULT_PAGE_ELEMENT_POSITION_VALUE = 'left';
   static DEFAULT_PAGE_SIZE_OPTIONS_VALUE: Array<number> = [20, 50, 100];
-  static DEFAULT_PAGE_SIZE_ELEMENT_POSITION_VALUE: string = 'right';
-  static DEFAULT_PAGE_SIZE_VALUE: number = 20;
-  static DEFAULT_PAGING_VALUE: boolean = true;
-  static DEFAULT_PRESERVE_SELECTION_VALUE: boolean = false;
-  static DEFAULT_REQUIRE_FILTERS: boolean = false;
-  static DEFAULT_ROW_ALTERNATE_STYLE_VALUE: boolean = true;
-  static DEFAULT_ROW_HOVER_STYLE_VALUE: boolean = true;
-  static DEFAULT_ROW_SELECTION_STYLE_VALUE: boolean = true;
-  static DEFAULT_SELECTION_VALUE: boolean = false;
-  static DEFAULT_SELECTION_MULTIPLE_VALUE: boolean = true;
-  static DEFAULT_SORTING_VALUE: boolean = true;
-  static DEFAULT_WIDTH_VALUE: string = '100%';
-  static DEFAULT_UNIQUE_ID_VALUE: string = 'id';
+  static DEFAULT_PAGE_SIZE_ELEMENT_POSITION_VALUE = 'right';
+  static DEFAULT_PAGE_SIZE_VALUE = 20;
+  static DEFAULT_PAGING_VALUE = true;
+  static DEFAULT_PRESERVE_SELECTION_VALUE = false;
+  static DEFAULT_REQUIRE_FILTERS = false;
+  static DEFAULT_ROW_ALTERNATE_STYLE_VALUE = true;
+  static DEFAULT_ROW_HOVER_STYLE_VALUE = true;
+  static DEFAULT_ROW_SELECTION_STYLE_VALUE = true;
+  static DEFAULT_SELECTION_VALUE = false;
+  static DEFAULT_SELECTION_MULTIPLE_VALUE = true;
+  static DEFAULT_SORTING_VALUE = true;
+  static DEFAULT_WIDTH_VALUE = '100%';
+  static DEFAULT_UNIQUE_ID_VALUE = 'id';
 
   protected additionalRequestParams: any;
   protected bodyCssClass: string;
